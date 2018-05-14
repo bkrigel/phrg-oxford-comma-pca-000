863c0189515bfc07548eedda9633c8778cc0df36
bkrigel <benjamin.krigel@gmail.com>
2018-05-14 16:18:54 -0400

    Hello

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  else array.count >= 3
+    last_element = array.last
+    array.pop
+    array.join(", ").insert(-1, ", and #{last_element}")
+  end
 end
